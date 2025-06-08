d38115639ef77e46fff46ae6756105f8933f277d
asthetik <asthetik.sun@outlook.com>
2025-06-08 15:45:10 +0800

    cargo fmt

--- counter/src/bin/mutil.rs
+++ counter/src/bin/mutil.rs
@@ -13,30 +13,32 @@ fn main() -> Result<(), Box<dyn Error>> {
             } else {
                 None
             }
-        }).collect();
-        let mut handles = vec![];
-        const CHUNK_SIZE: usize = 5;
-        let chunks = files.chunks(CHUNK_SIZE);
-        for chunk in chunks {
-            let mut local_map = HashMap::new();
-            let chunk = chunk.to_vec();
-            let handle = thread::spawn(move || {
-                chunk.iter()
-                    .filter_map(|p| fs::read_to_string(p).ok())
-                    .for_each(|text| {
-                        text.split_whitespace().for_each(|w| {
-                            let word = w
-                                .trim_matches(|c: char| c.is_ascii_punctuation())
-                                .to_lowercase();
-                            if !word.is_empty() {
-                                *local_map.entry(word).or_insert(0) += 1;
-                            }
-                        });
+        })
+        .collect();
+    let mut handles = vec![];
+    const CHUNK_SIZE: usize = 5;
+    let chunks = files.chunks(CHUNK_SIZE);
+    for chunk in chunks {
+        let mut local_map = HashMap::new();
+        let chunk = chunk.to_vec();
+        let handle = thread::spawn(move || {
+            chunk
+                .iter()
+                .filter_map(|p| fs::read_to_string(p).ok())
+                .for_each(|text| {
+                    text.split_whitespace().for_each(|w| {
+                        let word = w
+                            .trim_matches(|c: char| c.is_ascii_punctuation())
+                            .to_lowercase();
+                        if !word.is_empty() {
+                            *local_map.entry(word).or_insert(0) += 1;
+                        }
                     });
-                local_map
-            });
-            handles.push(handle);
-        }
+                });
+            local_map
+        });
+        handles.push(handle);
+    }
 
     for h in handles {
         let local_map = h.join().unwrap();
